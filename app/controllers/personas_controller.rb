b44c7ad25f983bc4d872f5b120465228590b2f68
Familia <andmej@gmail.com>
2008-06-02 15:02:20 -0500

    Mejorada vista de persona.

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -11,6 +11,17 @@ class PersonasController < ApplicationController
     end
   end
 
+  # GET /personas/1
+  # GET /personas/1.xml
+  def show
+    @persona = Persona.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @alumno }
+    end
+  end
+
   def new
     @persona = Persona.new
     respond_to do |format|
@@ -18,6 +29,12 @@ class PersonasController < ApplicationController
       format.xml { render :xml => @persona }
     end
   end
+
+  # GET /personas/1/edit
+  def edit
+    @persona = Persona.find(params[:id])
+  end
+
 
   def create
     @persona = Persona.new(params[:persona])
@@ -33,5 +50,34 @@ class PersonasController < ApplicationController
       end
     end
 
+  end
+
+  # PUT /personas/1
+  # PUT /personas/1.xml
+  def update
+    @persona = Persona.find(params[:id])
+
+    respond_to do |format|
+      if @persona.update_attributes(params[:persona])
+        flash[:notice] = 'Persona was successfully updated.'
+        format.html { redirect_to(@persona) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @persona.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /alumnos/1
+  # DELETE /alumnos/1.xml
+  def destroy
+    @persona = Persona.find(params[:id])
+    @persona.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(personas_url) }
+      format.xml  { head :ok }
+    end
   end
 end

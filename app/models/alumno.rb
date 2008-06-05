cd3e4e5f1ed2562ddc06722678645598f0d77439
Familia <andmej@gmail.com>
2008-06-05 13:52:56 -0500

    Agregado edad a alumno

--- app/models/alumno.rb
+++ app/models/alumno.rb
@@ -2,10 +2,24 @@
 # fecha_nacimiento, fecha_ingreso, cuidados_especiales
 class Alumno < Persona
   validates_presence_of :fecha_nacimiento, :fecha_ingreso, :cuidados_especiales
+  validates_date :fecha_nacimiento, :before => Date.today
+
   belongs_to :familia
   belongs_to :grupo
   belongs_to :ruta
   has_one :exalumno, :dependent => :destroy, :foreign_key => :persona_id
+
+
+  # Retorna la edad de un alumno, en años cumplidos
+  def edad
+    t = Date.today
+    f = fecha_nacimiento
+    if t.month < f.month or (t.month == f.month and t.day < f.day) # No ha cumplido años todavía
+      t.year - f.year - 1
+    else
+      t.year - f.year
+    end
+  end
 
   # Busca los alumnos sin familia y permite filtrar por medio de sus nombres y apellidos su busqueda
   def self.alumnos_sin_familia(campo)
@@ -25,7 +39,7 @@ class Alumno < Persona
                            :order => "apellidos, nombres ASC")
     @alumnos.delete_if{ |a| not a.exalumno.nil? }
   end
-  
+
   # Busca alumnos por medio de sus nombres y apellidos
   def self.search(campo)
     s = limpiar_string_buscadora(campo)
